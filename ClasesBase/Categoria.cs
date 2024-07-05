4771470d8edea244e11a888b52bcfd080ec6acae
Mauricio Miranda <129995731+MauricioMiranda6030@users.noreply.github.com>
2024-07-05 01:59:37 -0300

    Entidades Categoria y Disciplina
    las entidades que me faltaban

---
+++ ClasesBase/Categoria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+        }
+
+        private int cat_ID;
+
+        public int Cat_ID
+        {
+            get { return cat_ID; }
+            set { cat_ID = value; }
+        }
+        private string cat_Nombre;
+
+        public string Cat_Nombre
+        {
+            get { return cat_Nombre; }
+            set { cat_Nombre = value; }
+        }
+        private string cat_Descripcion;
+
+        public string Cat_Descripcion
+        {
+            get { return cat_Descripcion; }
+            set { cat_Descripcion = value; }
+        }
+
+        public override string ToString()
+        {
+            string categoriaString = "ID: " + cat_ID + "\n" +
+                                     "Nombre Categoria: " + cat_Nombre + "\n" +
+                                     "Descripcion: " + cat_Descripcion;
+            return categoriaString;
+        }
+    }
+}
---
+++ ClasesBase/Disciplina.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Disciplina
+    {
+        private static int idCont = 1;
+
+        public Disciplina()
+        {
+            this.dis_ID = idCont;
+            idCont++;
+        }
+
+        private int dis_ID; /*Preguntar si se puede cambiar a entero*/
+
+        public int Dis_ID
+        {
+            get { return dis_ID; }
+            set { dis_ID = value; }
+        }
+        private string dis_Nombre;
+
+        public string Dis_Nombre
+        {
+            get { return dis_Nombre; }
+            set { dis_Nombre = value; }
+        }
+        private string dis_Descripcion;
+
+        public string Dis_Descripcion
+        {
+            get { return dis_Descripcion; }
+            set { dis_Descripcion = value; }
+        }
+
+        public override string ToString()
+        {
+            string disciplinaString = "ID: " + Dis_ID + "\n" +
+                                  "Nombre Disciplina: " + Dis_Nombre + "\n" +
+                                  "Descripcion: " + Dis_Descripcion;
+            return disciplinaString;
+        }
+
+    }
+}
