1e6f1781796b08573630edc42e6df989266a71d7
Gutierrez Angel Gonzalo <165941854+cher-ange@users.noreply.github.com>
2024-07-10 07:37:02 -0300

    Más cosas

--- ClasesBase/TrabajarCronometraje.cs
+++ ClasesBase/TrabajarCronometraje.cs
@@ -40,10 +40,10 @@ namespace ClasesBase
         {
             string sqlQuery = @"
                 SELECT
-                    Evento.Eve_ID,
-                    Evento.Eve_HoraInicio,
-                    Evento.Eve_HoraFin,
-                    Evento.Eve_Estado
+                    Evento.Eve_ID AS 'Id',
+                    Evento.Eve_HoraInicio AS 'Hora Inicio',
+                    Evento.Eve_HoraFin AS 'Hora Fin',
+                    Evento.Eve_Estado AS 'Estado'
                 FROM
                     Atleta
                 INNER JOIN Evento ON Atleta.Atl_ID = Evento.Atl_ID
@@ -73,27 +73,27 @@ namespace ClasesBase
             }
         }
 
-        public static DataTable UpdateHoraFinEvent(string pattern)
+        public static void UpdateHoraFinEvent(int eve_Id, DateTime hora_Inicio, DateTime hora_Fin)
         {
             string sqlQuery = @"
-                
+                UPDATE Evento
+                SET
+                    Eve_HoraInicio = @HoraInicio,
+                    Eve_HoraFin = @HoraFin
+                WHERE
+                    Eve_ID = @Eve_Id
             ";
             using (sqlConnection = new SqlConnection(connectionString))
             {
-                using (sqlCommand = new SqlCommand("BuscarAtletaSegunPatron", sqlConnection))
+                using (sqlCommand = new SqlCommand(sqlQuery, sqlConnection))
                 {
                     sqlConnection.Open();
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    sqlCommand.Parameters.AddWithValue("@Pattern", "%" + pattern + "%");
-
-                    using (sqlDataAdapter = new SqlDataAdapter(sqlCommand))
-                    {
-                        DataTable dataTable = new DataTable();
-
-                        sqlDataAdapter.Fill(dataTable);
-
-                        return dataTable;
-                    }
+                    //sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.CommandType = CommandType.Text;
+                    sqlCommand.Parameters.AddWithValue("@Eve_Id", eve_Id);
+                    sqlCommand.Parameters.AddWithValue("@HoraInicio", hora_Inicio);
+                    sqlCommand.Parameters.AddWithValue("@HoraFin", hora_Fin);
+                    sqlCommand.ExecuteNonQuery();
                 }
             }
         }
